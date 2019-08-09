6d011bf5f7a0bbe25cc693fa7f159d589766bdf0
Valerii Kuzivanov <valerii.kuzivanov@gmail.com>
2019-08-09 12:49:43 +0300

    ant design added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import Button from 'antd/es/button';
+import './App.css';
 
 export default () => (
   <div className="App">
-
+    <Button type="primary">Button</Button>
   </div>
 );
