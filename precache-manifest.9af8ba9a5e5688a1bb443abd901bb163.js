0e4a8cc2b591a6714725f6c242da1525336f7fac
Valerii Kuzivanov <valerii.kuzivanov@gmail.com>
2019-08-10 19:34:19 +0300

    Updates

---
+++ precache-manifest.9af8ba9a5e5688a1bb443abd901bb163.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c781ad000f18b7342eaa417224b7a5a",
+    "url": "/Valeronlol/syn_t/index.html"
+  },
+  {
+    "revision": "d67bf95b8dd71034fd71",
+    "url": "/Valeronlol/syn_t/static/css/4.0ba87e64.chunk.css"
+  },
+  {
+    "revision": "4c20dd9656eb482c8ecb",
+    "url": "/Valeronlol/syn_t/static/css/main.c274eac3.chunk.css"
+  },
+  {
+    "revision": "33e116f18e9af6299bc7",
+    "url": "/Valeronlol/syn_t/static/js/0.d18ba260.chunk.js"
+  },
+  {
+    "revision": "71ab080878b4e7d07580",
+    "url": "/Valeronlol/syn_t/static/js/1.bb940b8f.chunk.js"
+  },
+  {
+    "revision": "d67bf95b8dd71034fd71",
+    "url": "/Valeronlol/syn_t/static/js/4.173ba791.chunk.js"
+  },
+  {
+    "revision": "4c20dd9656eb482c8ecb",
+    "url": "/Valeronlol/syn_t/static/js/main.20ad71ac.chunk.js"
+  },
+  {
+    "revision": "ced77e1a4d7eab6aa450",
+    "url": "/Valeronlol/syn_t/static/js/runtime~main.89d17a8f.js"
+  }
+]);
