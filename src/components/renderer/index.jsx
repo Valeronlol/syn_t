758c812cacebd93cffc90a4617deef66f79a7ad6
Valerii Kuzivanov <valerii.kuzivanov@gmail.com>
2019-08-10 18:31:23 +0300

    youtube embed iframe

--- src/components/renderer/index.jsx
+++ src/components/renderer/index.jsx
@@ -1,6 +1,6 @@
 import React, { Suspense } from 'react';
 import PropTypes from 'prop-types';
-import { Row, Empty } from 'antd';
+import { Row, Empty, Spin } from 'antd';
 
 const EmptyTemplate = (
   <Empty
@@ -17,7 +17,7 @@ const Renderer = ({ blocks, currentSource }) => {
   const getTemplate = (props, idx) => {
     const Component = React.lazy(() => import(`./source-templates/${currentSource}`));
     return (
-      <Suspense key={idx} fallback={<div>Loading...</div>}>
+      <Suspense key={idx} fallback={<Spin/>}>
         <Component {...props}/>
       </Suspense>
     )
@@ -26,10 +26,7 @@ const Renderer = ({ blocks, currentSource }) => {
   return (
     <div className="renderer-container">
       <Row type="flex" justify="space-around" align="middle">
-        {blocks.length > 0
-          ? blocks.map(getTemplate)
-          : EmptyTemplate
-        }
+        {blocks.length > 0 ? blocks.map(getTemplate) : EmptyTemplate}
       </Row>
     </div>
   )
--- src/components/renderer/source-templates/youtube.js
+++ src/components/renderer/source-templates/youtube.js
@@ -1,16 +1,47 @@
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useState } from 'react';
 import { Col } from "antd";
 
-const YouTubeTemplate = ({ snippet, id }) => (
-  <Col span={6} key={id} className='renderer-el'>
-    <img
-      src={snippet.thumbnails.high.url}
-      alt={snippet.title}
-      className='renderer-image'
-    />
-  </Col>
-);
+const YouTubeTemplate = ({ snippet, id }) => {
+  const [isShowIFrame, setIFrameStatus] = useState(false);
+
+  const startVideo = () => setIFrameStatus(true);
+
+  return (
+    <Col span={6} key={id} className='renderer-el'>
+      {isShowIFrame
+        ? (
+          <iframe
+            title='yt-frame'
+            className='renderer-el-ytplayer'
+            type='text/html'
+            src={`https://www.youtube.com/embed/${id}?autoplay=1`}
+            frameBorder='0'
+            allow="autoplay; encrypted-media"
+            allowFullScreen
+          />
+        )
+        : (
+          <>
+            <img
+              src={snippet.thumbnails.high.url}
+              alt={snippet.title}
+              className='renderer-image'
+            />
+            <div className='renderer-el-overflow'>
+              <img
+                className='yt-play-icon'
+                src='/yt_play.png'
+                alt='play'
+                onClick={startVideo}
+              />
+            </div>
+          </>
+        )
+      }
+    </Col>
+  )
+};
 
 YouTubeTemplate.propTypes = {
   snippet: PropTypes.object.isRequired,
